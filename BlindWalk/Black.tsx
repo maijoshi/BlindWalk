8e676d4b6e7ed0eae336d579b89619add1ab004d
Maitreyee Joshi <maitreyee.joshi@gmail.com>
2017-10-29 14:57:27 -0400

    blindwalk

---
+++ BlindWalk/Black.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Black" tilewidth="32" tileheight="32" tilecount="225" columns="15">
+ <image source="../../../../Desktop/Black.png" width="480" height="480"/>
+</tileset>
---
+++ BlindWalk/Tan.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Tan" tilewidth="32" tileheight="32" tilecount="1221" columns="37">
+ <image source="../../../../Desktop/Tan.png" width="1200" height="1056"/>
+</tileset>
